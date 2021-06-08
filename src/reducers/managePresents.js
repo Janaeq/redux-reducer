a9a66ddf37e4b90b115b32d24ea6f4246451cfb3
Janae Quinones <janaemassa@gmail.com>
2021-06-07 20:08:47 -0400

    commit tree 601bfde2c97030f72e2b77b4b0b48e7ee19a6473

--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,2 +1,9 @@
-export function managePresents(state, action){
+// Function takes in previous state and an action as arg
+export function managePresents(state = { numberOfPresents: 0 }, action){
+    switch (action.type) {
+        case 'INCREASE':
+            return { numberOfPresents: state.numberOfPresents +1 }
+        default:
+            return state
+    }
 }
